e22dd002c31fda1bc23a5d07711426868a95f959
shimomu919 <68220070+shimomu919@users.noreply.github.com>
2020-10-05 12:36:23 +0900

    Add files via upload

---
+++ kitasai_2020/CountDown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountDown : MonoBehaviour
+{
+	public Text CountTime;
+	public float time;
+	int seconds;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		time -= Time.deltaTime;
+		seconds = (int)time;
+		CountTime.text = seconds.ToString();
+		if(seconds == 0)
+		{
+			SceneManager.LoadScene("GameOver");
+		}
+	}
+}
