25f50ac0b371572f137d8e61665ce73076a2ce77
shimomu919 <68220070+shimomu919@users.noreply.github.com>
2020-10-15 17:04:07 +0900

    Add files via upload

---
+++ kitasai_2020/Jump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Jump : MonoBehaviour
+{
+	public GameObject PlayerController;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		PlayerController = GameObject.Find("Player");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	public void onClick()
+	{
+		PlayerController.GetComponent<PlayerController>().mvJump = true;
+	}
+}
--- kitasai_2020/PlayerController.cs
+++ kitasai_2020/PlayerController.cs
@@ -11,17 +11,13 @@ public class PlayerController : MonoBehaviour
 
 	public string blockTag = "Block";
 
-	// public GameObject textObject;
+	public bool mvJump = false;
 
-	void Start()
-	{
-		// textObject = GameObject.Find("Timer");
-	}
+	// public GameObject textObject;
 
 	// 当たり判定
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		
 		if(life == 0)
 		{
 			if(collision.collider.tag == blockTag)
@@ -37,14 +33,30 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
+	void Start()
+	{
+		// textObject = GameObject.Find("Timer");
+		mvJump = false;
+	}
+
 	void Update()
 	{
 		/* if(textObject.GetComponent<TimerController>().seconds > 0)
 		{
 			return;
 		}*/
-		
+
 		if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+		{
+			if(jump == 0 || jump == 1)
+			{
+				transform.Translate(speed * Time.deltaTime, speed * Time.deltaTime, 0);
+				jump++;
+			}else
+			{
+				transform.Translate(speed * Time.deltaTime, 0, 0);
+			}
+		} else if(mvJump == true)
 		{
 			if(jump == 0 || jump == 1)
 			{
